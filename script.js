82f4e1a9edf257640d3ec05c6b38041af8c39752
Muluken <mulerthegrete@gmail.com>
2021-11-08 16:50:28 -0500

    javaScript file created and variables added

---
+++ script.js
@@ -0,0 +1,27 @@
+// Button variables
+let numberNine = document.getElementById("btn-nine")
+let numberEight = document.getElementById("btn-eight")
+let numberSeven = document.getElementById("btn-seven")
+let numberSix = document.getElementById("btn-six")
+let numberFive = document.getElementById("btn-five")
+let numberFour = document.getElementById("btn-four")
+let numberThree = document.getElementById("btn-three")
+let numberTwo = document.getElementById("btn-two")
+let numberOne = document.getElementById("btn-one")
+let numberZero = document.getElementById("btn-zero")
+
+
+
+
+// Operator varables
+let plus = document.getElementById("btn-plus")
+let minus = document.getElementById("btn-minus")
+let multiply = document.getElementById("btn-multiply")
+let devide = document.getElementById("btn-devide") 
+let equals = document.getElementById("btn-equalls")
+
+
+// The result form
+let results = document.getElementById("result")
+let btnResult = document.getElementById("btn-result")
+let textResult = document.getElementById("text-result")
